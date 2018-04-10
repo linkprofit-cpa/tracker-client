71589dc491ea3637bbf2d45dfbfd2094ec80623d
phrlog <kh@linkprofit.com>
2018-04-10 14:25:21 +0300

    добавил filters

---
+++ src/filter/FilterBuilderInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace linkprofit\Tracker\filter;
+
+use linkprofit\Tracker\request\RequestContentInterface;
+
+/**
+ * Interface FilterBuilderInterface
+ */
+interface FilterBuilderInterface
+{
+    /**
+     * Должен возвращать ассоциативный массив, где key это название фильтра, а value это его значение
+     *
+     * @return array
+     */
+    public function toArray();
+
+    /**
+     * @return RequestContentInterface
+     */
+    public function createRequestContent();
+}
