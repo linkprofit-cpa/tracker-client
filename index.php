d76edd71f5e8ea87f40d63670cfb814cf6f1f7d4
phrlog <kh@linkprofit.com>
2018-04-10 14:26:27 +0300

    добавил index.php с примером использования

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once "vendor/autoload.php";
+
+$connection = new \linkprofit\Tracker\Connection();
+
+$connection->userName = '';
+$connection->userPassword = '';
+$connection->apiUrl = '';
+$connection->accessLevel = \linkprofit\Tracker\AccessLevel::ADMIN;
+
+$client = new \linkprofit\Tracker\Client($connection);
+$client->connect();
+
+$offers = new \linkprofit\Tracker\filter\OffersFilterBuilder();
+$offers = $offers->isActive()->limit(10)->createRequestContent();
+
+$response  = $client->exec($offers);
+
+var_dump($response->toArray());
