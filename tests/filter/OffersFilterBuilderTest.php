db84ac110c4a4f6653cc689ebea3a314f8cfafec
phrlog <kh@linkprofit.com>
2018-04-13 13:37:38 +0300

    убрал недоступный метод notHidden в OffersFilterBuilder

--- tests/filter/OffersFilterBuilderTest.php
+++ tests/filter/OffersFilterBuilderTest.php
@@ -11,14 +11,14 @@ class OffersFilterBuilderTest extends TestCase
         $builder = new OffersFilterBuilder();
         $this->assertEquals([], $builder->toArray());
 
-        $builder->categoryId(1)->isActive()->limit(10)->notHidden()->offset(20)->orderByField('field');
-        $this->assertEquals(['categoryId' => 1, 'active' => 1, 'hidden' => 0, 'limit' => 10, 'offset' => 20, 'orderByField' => 'field'], $builder->toArray());
+        $builder->categoryId(1)->isActive()->limit(10)->offset(20)->orderByField('field');
+        $this->assertEquals(['categoryId' => 1, 'active' => 1, 'limit' => 10, 'offset' => 20, 'orderByField' => 'field'], $builder->toArray());
     }
 
     public function testCreateRequestContent()
     {
         $builder = new OffersFilterBuilder();
-        $builder->categoryId(1)->isActive()->limit(10)->notHidden()->offset(20)->orderByField('field');
+        $builder->categoryId(1)->isActive()->limit(10)->offset(20)->orderByField('field');
 
         $this->assertInstanceOf(OffersRequestContent::class, $builder->createRequestContent());
     }
