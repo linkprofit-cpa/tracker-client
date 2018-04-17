830d42d4c3e3d7a282223d9541640688095d50b9
phrlog <kh@linkprofit.com>
2018-04-17 14:41:37 +0300

    обновил тесты

--- tests/builder/ReadOffersBuilderTest.php
+++ tests/builder/ReadOffersBuilderTest.php
@@ -11,8 +11,8 @@ class ReadOffersBuilderTest extends TestCase
         $builder = new ReadOffersBuilder();
         $this->assertEquals([], $builder->toArray());
 
-        $builder->categoryId(1)->isActive()->limit(10)->offset(20)->orderByField('field');
-        $this->assertEquals(['categoryId' => 1, 'active' => 1, 'limit' => 10, 'offset' => 20, 'orderByField' => 'field'], $builder->toArray());
+        $builder->categoryId(1)->isActive()->limit(10)->offset(20)->orderByField('field')->merchantManagerId(2)->mainFilterItem('term');
+        $this->assertEquals(['categoryId' => 1, 'active' => 1, 'limit' => 10, 'offset' => 20, 'orderByField' => 'field', 'merchantManagerId' => 2, 'mainFilterItem' => 'term'], $builder->toArray());
     }
 
     public function testCreateRequestContent()
